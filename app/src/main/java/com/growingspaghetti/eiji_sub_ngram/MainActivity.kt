932207dda2ea5294b236bd63a73395ba96fe57bc
Ryoji <ryoji.kodakari@gmail.com>
2023-03-13 08:12:57 +0900

    initial commit

---
+++ app/src/main/java/com/growingspaghetti/eiji_sub_ngram/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.growingspaghetti.eiji_sub_ngram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
